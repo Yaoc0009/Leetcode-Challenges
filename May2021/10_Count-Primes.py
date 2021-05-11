333d9898c9013d46661224bc95d563f1244c51ef
qinghwee <74318228+Yaoc0009@users.noreply.github.com>
2021-05-11 13:43:57 +0800

    Create 10_Count-Primes.py

---
+++ May2021/10_Count-Primes.py
@@ -0,0 +1,20 @@
+
+# Simple solution based on Sieve of Eratosthenes
+# Time Complexity: O(n log log n)
+# Space Complexity: O(n)
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 2:
+            return 0
+            
+        arr = [1]*n
+        arr[0],arr[1] = 0,0
+        
+        for i in range(2,int(math.sqrt(n))+1):
+            if arr[i] == 1:
+                for j in range(i**2,n,i):
+                    arr[j] = 0
+                    
+        return sum(arr)
+            
